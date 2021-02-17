90771a8a9ca6322e084c02f8be01a3d342ad1f64
SaiDineshYadav <boddusaidineshyadav@gmail.com>
2021-02-17 16:34:01 +0530

    Dinesh added Instagram example

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header bordered>
+      <q-toolbar>
+        <q-btn 
+        to="/camera"
+        size="18px"
+        icon="eva-camera-outline"
+        flat round dense
+         />
+         <q-separator vertical spaced />
+        <q-toolbar-title class="text-center">
+          Quasagram
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white">
+      <q-tabs class="text-grey-10"
+      active-color="primary"
+      indicator-color="transparent"> 
+        <q-route-tab
+          icon="eva-home-outline"
+          to="/"
+          label="Home"
+          exact
+        />
+        <q-route-tab
+          icon="eva-camera-outline"
+          to="/camera"
+          label="Camera"
+          exact
+        />
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container class="bg-grey-1">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false,
+    }
+  }
+}
+</script>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,114 @@
+<template>
+  <q-page class="constrain q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-8">
+        <q-card
+          class="card-post"
+          v-for="post of posts"
+          :key="post.id"
+          flat
+          bordered>
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold" >Title</q-item-label>
+              <q-item-label caption>
+                Subhead
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+          <q-img
+            :src="post.imageUrl"
+          />
+
+        <q-card-section>
+            <div class="text-h6">{{post.caption}}</div>
+            <div class="text-caption text-grey">
+              {{ post.date | getDateFormat}}
+            </div>
+          </q-card-section>
+
+        </q-card>
+      </div>
+      <div class="col-4">
+        <q-item class="fixed">
+            <q-item-section avatar>
+              <q-avatar size="48px">
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold" >Title</q-item-label>
+              <q-item-label caption>
+                Subhead
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+      </div>
+
+    </div>
+
+  
+  </q-page>
+</template>
+
+<script>
+import { date } from 'quasar'
+
+export default {
+  name: 'PageHome',
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: 'Caption 1',
+          date: 1613545542586,
+          location: 'Vizag',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+         {
+          id: 2,
+          caption: 'Caption 1',
+          date: 1613545542586,
+          location: 'Vizag',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+         {
+          id: 3,
+          caption: 'Caption 1',
+          date: 1613545542586,
+          location: 'Vizag',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+         {
+          id: 4,
+          caption: 'Caption 1',
+          date: 1613545542586,
+          location: 'Vizag',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+      ]      
+    }    
+  },
+  filters: {
+        getDateFormat(value) {
+          return date.formatDate(value, 'MMMM D h:mm A')
+        }
+      }
+}
+</script>
+
+<style lang="sass">
+  .card-post
+    .q-img
+      min-height: 200px
+</style>
